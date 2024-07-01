a61450531d9257fd04c48ae3997922beecfe82ad
sameer singh <v-samesingh@microsoft.com>
2024-07-01 18:14:45 +0530

    Add project files.

---
+++ EnumProject/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+namespace EnumProject
+{
+    public enum Days
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday
+    }
+    class Program
+    {
+        public static Days MeetingDay { get; set; } = Days.Monday;
+        static void Main()
+        {
+            Days d=Days.Wednesday;
+            Console.WriteLine(d);
+            Console.WriteLine((int)d);
+
+            foreach(int i in Enum.GetValues(typeof(Days)))     //GetValued() method returns an array of the values of the constants in a specified enumeration
+            {
+                Console.WriteLine(i);
+            }
+
+            foreach (string s in Enum.GetNames(typeof(Days)))   //GetNames() method returns an array of the names of the constants in a specified enumeration
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.WriteLine(MeetingDay);
+            MeetingDay=Days.Friday;
+            Console.WriteLine(MeetingDay);
+
+
+        }
+    }
+
+}
